cf4d4f734cdade3a6d6402cb13accd22f1629b3f
idk <idk@laptop.idk.i2p>
2020-09-15 00:52:23 -0400

    correct setting of keyname, sam bridge in Listen

--- lib/serve.go
+++ lib/serve.go
@@ -66,7 +66,7 @@ var Options_DChat_Short = []string{"inbound.length=1", "outbound.length=1", "inb
 func Listen(yoursam, certfile, keyfile string) net.Listener {
 	opts := make(map[string]string)
 	opts[`sam`] = yoursam
-	opts[`servertun`] = "dirc2p" + sammy.RandStringBytes()
+	opts[`servertun`] = "dirc2p"
 	opts[`keypath`] = opts[`servertun`]+".i2pkeys"
 	ln, err := sammy.Sammy(opts)
 	if err != nil {
